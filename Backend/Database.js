bccce362c43bf16c840d5ec80d91208a5ae61cd7
SelasiMadeItHere <selasiprecious20@gmail.com>
2023-04-29 03:32:05 +0000

    29/04/2023-3:30

--- Backend/Database.js
+++ Backend/Database.js
@@ -1,8 +1,27 @@
 const express = require('express');
-const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require("cors")
+const mysql2 = require('mysql2');
 const app = express();
 
-const connection = mysql.createConnection({
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+  res.send("Helloo Express on Aviola"); 
+})
+
+const PORT =5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
+
+const connection = mysql2.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
@@ -11,24 +30,21 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Connected to database!');
+  console.log('Connected to database! Hooray!!!!');
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-app.post('/api/user', (req, res) => {
-  const { fname, sname, age, locations, gender, telephone } = req.body;
-  const query = `INSERT INTO tryouts (fname, sname, age, locations, gender, telephone) VALUES (?, ?, ?, ?, ?, ?)`;
-  connection.query(query, [fname, sname, age, location, gender, telephone], (err, result) => {
-    if (err) throw err;
-    console.log('Data inserted successfully!');
-    res.send('Data inserted successfully!');
-  });
-});
 
-const PORT =5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.post('/api/user', (req, res) => {
+//   const { fname, sname, age, locations, gender, telephone } = req.body;
+//   const query = `INSERT INTO tryouts (fname, sname, age, locations, gender, telephone) VALUES (?, ?, ?, ?, ?, ?)`;
+//   connection.query(query, [fname, sname, age, location, gender, telephone], (err, result) => {
+//     if (err) throw err;
+//     console.log('Data inserted successfully!');
+//     res.send('Data inserted successfully!');
+//   });
+// });
+
+
+
